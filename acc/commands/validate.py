ff52e4983438f103bdf56434b73daa9308591de2
Pedro Juarez <pjuarez@tiempodevelopment.com>
2017-09-25 16:05:46 -0500

    Tests descriptor file over a relative or absolute path

--- acc/commands/validate.py
+++ acc/commands/validate.py
@@ -1,21 +1,35 @@
 """The validate command."""
 
-from json import dumps
 from .base import Base
 import os.path
 import yaml
+import jsonschema
 
 class Validate(Base):
 
     def run(self):
         if self.options['<file>'] is None:
-        	file = './descriptor.yml'
+        	file = 'descriptor.yml'
         else:
-        	file = "./" + self.options['<file>'] 
-        if os.path.exists(file) and os.path.isfile(file):
-        	with open(file, 'r') as f:
-    			doc = yaml.load(f)
-    	else:
-    		print("file '" + file + "' or desciptor.yml does not exists, exiting")
+        	file = self.options['<file>']
+        
+        absPath = self.getAbsolutePath(file)
 
-    	print doc['acc-version']
+        if os.path.exists(absPath) and os.path.isfile(absPath):
+        	with open(absPath, 'r') as f:
+    			doc = yaml.load(f)
+                print doc['acc-version']
+    	else:
+            if self.options['<file>'] is None:
+                print("file 'desciptor.yml' does not exists, exiting")
+            else:
+                print("file '" + self.options['<file>'] + "' does not exists, exiting")
+
+    @staticmethod
+    def getAbsolutePath(pathString):
+        dir = os.getcwd()
+        if os.path.isabs(pathString):
+            absPath = pathString
+        else:
+            absPath = os.path.join(dir, pathString)
+        return absPath
